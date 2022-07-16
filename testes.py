248c54822e023ec99ed46e4bb30b0e890effe2ce
felipe willy <felipe00cj@gmail.com>
2022-07-16 15:36:11 -0300

    Aplicando tentativas com DATETIME

--- testes.py
+++ testes.py
@@ -1,5 +1,5 @@
 import json
-
+from datetime import datetime, timedelta, time
 
 
 class Cascudo:
@@ -19,12 +19,72 @@ class Cascudo:
             self.vasco.append(arquivo_at)
             return self.vasco
             
-       
+t = Cascudo()
+atualiza = t.abrir()
+saida = t.atualiz(atualiza)
+soma = (saida[0]['Ataque'] + saida[0]['Posse'] + saida[1]['povo'] + saida[1]['robo']) / 4
+print(f'{soma:.2f}')
 
-time = Cascudo()
-atualiza = time.abrir()
-saida = time.atualiz(atualiza)
-for i in saida[:]:
-    for k, v in i.items():
-        print(k, v)
+class Altomatizou():
+    def __init__(self, hora):
+        self.hora = hora
+        self.hora_now = datetime.now().time()
+    
+  
+    #getter
+    @property
+    def hora(self):
+        return self._hora
+    #setter
+    @hora.setter
+    def hora(self, valor):
+        hora = valor
+        horac = str(hora).split(' ')
+        horad = horac[-1]
+        horae = datetime.strptime(horad, "%H:%M:%S").time()
+        horae = timedelta(hours=13)
+        self.horaf = horae
+        self._hora = self.horaf
 
+    def bloco_one(self):
+        self.hora_now = datetime.now().time()
+        self.bloco_set = self.horaf + timedelta(minutes=45)
+        self.bloco_sets = self.bloco_set + timedelta(minutes=15)
+        self.bloco_setss = self.bloco_sets + timedelta(minutes=45)
+        print(f'HORA JOGO {self.horaf}')
+        print(f'HORA 1T:  {self.bloco_set}')
+        print(f'HORA 2T:  {self.bloco_sets}')
+        print(f'HOR CABO: {self.bloco_setss}')
+        print(self.hora_now)
+
+
+        if self.hora_now > self.horaf: 
+            print('Inicio do jogo: VERDADEIRO')
+        elif self.bloco_set >= self.hora_now:
+            print('Intervalo do jogo: FALSO')
+        else:
+            print('JOGO FORA DE HORA')
+
+
+
+
+
+
+
+''''
+    def bloco_one(self):
+        self.hora_now = datetime.now()
+        self.bloco_set = self.horaf + timedelta(minutes=45)
+        self.bloco_sets = self.bloco_set + timedelta(minutes=15)
+        self.bloco_setss = self.bloco_sets + timedelta(minutes=45)
+        print(self.horaf)
+        print(self.hora_now)
+        print(self.bloco_set)
+        print(self.bloco_sets)
+        print(self.bloco_setss)
+
+
+
+'''
+barcelona = Altomatizou(saida[0]['HC'])
+print(barcelona.bloco_one())
